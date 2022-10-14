af64eed1c2f99bb79170411bd0dc1cc1457640cd
ashish95426 <115772483+ashish95426@users.noreply.github.com>
2022-10-14 11:31:27 +0530

    Add files via upload

---
+++ 12oct/PrintDetails.java
@@ -0,0 +1,12 @@
+class PrintDetails
+{
+  public static void main(String args[])
+  {
+    System.out.println("name: "+args[0]);
+    System.out.println("age: "+args[1]);
+    System.out.println("course: "+args[2]);
+    System.out.println("Grade: "+args[3]);
+    
+  }
+
+}
---
+++ 12oct/PrintName.java
@@ -0,0 +1,8 @@
+class PrintName
+{
+   public static void main(String args[])
+   {
+     System.out.println("welcome " + args[0]+" "+args[1]);
+   }
+
+}
